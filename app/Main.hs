0bae4fb636d56a07f5189bf5d074828ef1603d50
ryndubei <114586905+ryndubei@users.noreply.github.com>
2023-04-10 22:33:01 +0100

    Hold-down controls

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,13 @@ import World.Shape (Shape)
 import Raymarch (runRaymarcher, Config(..))
 import qualified Linear as L
 import Graphics.Gloss.Interface.Pure.Game (Event (EventKey), Key (..), KeyState (..), SpecialKey (..))
+import qualified Data.Set as S
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
 
 windowWidth, windowHeight :: Int
-windowWidth = 800
-windowHeight = 800
+windowWidth = 600
+windowHeight = 600
 
 window :: Display
 window = InWindow "Raymarcher" (windowWidth, windowHeight) (0, 0)
@@ -20,6 +23,7 @@ data CameraState = CameraState
   , cameraRotation :: Double
   , cameraShape :: Shape
   , cameraSun :: Double
+  , pressedKeys :: S.Set Key
   }
 
 initialCameraState :: CameraState
@@ -28,17 +32,18 @@ initialCameraState = CameraState
   , cameraRotation = 0
   , cameraShape = yellowSphereOnPlaneGreenCube
   , cameraSun = 0
+  , pressedKeys = S.empty
   }
 
 cameraDirection :: CameraState -> L.V3 Double
 cameraDirection CameraState{cameraRotation} = L.rotate (L.axisAngle (L.V3 0 1 0) cameraRotation) (L.V3 0 0 1)
 
 main :: IO ()
-main = 
-  playField window (6,6) 10 initialCameraState getColourAtPoint handleKeys (const id)
+main =
+  playField window (7,7) 15 initialCameraState getColourAtPoint handleKeys update
 
 getColourAtPoint :: CameraState -> (Float, Float) -> Color
-getColourAtPoint CameraState{cameraShape, cameraPosition, cameraRotation, cameraSun} (x, y) = runRaymarcher Config 
+getColourAtPoint CameraState{cameraShape, cameraPosition, cameraRotation, cameraSun} (x, y) = runRaymarcher Config
   { maxSteps = 100
   , maxDistanceSq = 100
   , epsilon = 0.001
@@ -51,15 +56,27 @@ getColourAtPoint CameraState{cameraShape, cameraPosition, cameraRotation, camera
   }
 
 handleKeys :: Event -> CameraState -> CameraState
-handleKeys (EventKey (Char 'w') Down _ _) state = state { cameraPosition = cameraPosition state + cameraDirection state }
-handleKeys (EventKey (Char 's') Down _ _) state = state { cameraPosition = cameraPosition state - cameraDirection state }
-handleKeys (EventKey (Char 'a') Down _ _) state = state { cameraPosition = cameraPosition state - L.cross (L.V3 0 1 0) (cameraDirection state) }
-handleKeys (EventKey (Char 'd') Down _ _) state = state { cameraPosition = cameraPosition state + L.cross (L.V3 0 1 0) (cameraDirection state) }
-handleKeys (EventKey (Char 'r') Down _ _) state = state { cameraPosition = cameraPosition state + L.V3 0 1 0 }
-handleKeys (EventKey (Char 'f') Down _ _) state = state { cameraPosition = cameraPosition state - L.V3 0 1 0 }
-handleKeys (EventKey (Char 'h') Down _ _) state = state { cameraRotation = cameraRotation state - 0.1 }
-handleKeys (EventKey (Char 'l') Down _ _) state = state { cameraRotation = cameraRotation state + 0.1 }
-handleKeys (EventKey (SpecialKey KeyLeft) Down _ _) state = state { cameraSun = cameraSun state + 0.1 }
-handleKeys (EventKey (SpecialKey KeyRight) Down _ _) state = state { cameraSun = cameraSun state - 0.1 }
+handleKeys (EventKey key Down _ _) state = state { pressedKeys = S.insert key (pressedKeys state) }
+handleKeys (EventKey key Up _ _) state = state { pressedKeys = S.delete key (pressedKeys state) }
 handleKeys _ state = state
 
+movementKeybindings :: M.Map Key (CameraState -> CameraState)
+movementKeybindings = M.fromList [(Char 'w', \state -> state { cameraPosition = cameraPosition state + cameraDirection state L.^* 0.5 })
+                                 ,(Char 's', \state -> state { cameraPosition = cameraPosition state - cameraDirection state L.^* 0.5 })
+                                 ,(Char 'a', \state -> state { cameraPosition = cameraPosition state - L.cross (L.V3 0 1 0) (cameraDirection state) L.^* 0.5 })
+                                 ,(Char 'd', \state -> state { cameraPosition = cameraPosition state + L.cross (L.V3 0 1 0) (cameraDirection state) L.^* 0.5 })
+                                 ,(Char 'r', \state -> state { cameraPosition = cameraPosition state + L.V3 0 0.5 0 })
+                                 ,(Char 'f', \state -> state { cameraPosition = cameraPosition state - L.V3 0 0.5 0 })
+                                 ,(Char 'h', \state -> state { cameraRotation = cameraRotation state - 0.1 })
+                                 ,(Char 'l', \state -> state { cameraRotation = cameraRotation state + 0.1 })
+                                 ,(SpecialKey KeyLeft, \state -> state { cameraSun = cameraSun state + 0.1 })
+                                 ,(SpecialKey KeyRight, \state -> state { cameraSun = cameraSun state - 0.1 })
+                                 ]
+
+update :: Float -> CameraState -> CameraState
+update _ = handleMove
+
+handleMove :: CameraState -> CameraState
+handleMove state@CameraState{pressedKeys} =
+  let f = S.foldr (\key f' -> fromMaybe id (M.lookup key movementKeybindings) . f') id pressedKeys
+   in f state
