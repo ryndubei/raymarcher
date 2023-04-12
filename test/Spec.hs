507230188edb20d3cae05bacc47fff0aac5e4b32
ryndubei <114586905+ryndubei@users.noreply.github.com>
2023-04-12 01:38:05 +0100

    Add test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import Raymarch
+import qualified Data.Array.Accelerate as A
+import qualified Data.Array.Accelerate.Linear as L
+import Data.Array.Accelerate.Data.Colour.Names
+import World (yellowSphere)
+import qualified Data.Array.Accelerate.Interpreter as Interp
+import Data.Array.Accelerate.Data.Colour.RGBA (packRGBA)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  let conf = Config 100 100 0.01 (A.lift $ L.V3 (0.0 :: Double) 0.0 1.0) (A.lift $ L.V3 (0.0 :: Double) 0.0 0.0) black (A.lift $ L.V3 1.0 0.5 (0.0::Double)) yellowSphere 0.1
+      f = runRaymarcher conf
+      arr = A.generate (A.constant A.Z) (const (packRGBA f))
+  putStrLn "Running test"
+  let d = Interp.run arr
+  print (A.indexArray d A.Z)
+  putStrLn "Done"
+
