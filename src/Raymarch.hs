e88eef14a4f8a0545ea94abaa080b72af86418ee
ryndubei <114586905+ryndubei@users.noreply.github.com>
2023-04-14 15:03:37 +0100

    Use RWS instead of State

--- src/Raymarch.hs
+++ src/Raymarch.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE RebindableSyntax #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module Raymarch (runRaymarcher, Config(..)) where
 
 import Prelude hiding ((||), (<), (>), (&&), not)
 import World.Shape
-import Control.Monad.State.Lazy
 import qualified Data.Array.Accelerate.Linear as AL
 import Data.Array.Accelerate.Linear ()
 import qualified Data.Array.Accelerate as A
 import Data.Array.Accelerate (ifThenElse, (||), (<), (>), not)
 import Data.Array.Accelerate.Data.Colour.RGBA (Colour, blend)
 import Data.Array.Accelerate.Data.Colour.Names (black)
+import Control.Monad.RWS.Lazy
 
 data Config = Config
   { maxSteps :: A.Exp Int
@@ -26,62 +27,71 @@ data Config = Config
 
 data RaymarcherState = RaymarcherState
   { directionVector :: A.Exp Point
-  , config :: Config -- ^ NOTE: immutable - we assume config does not get modified at any point
   , positionVector :: A.Exp Point
   , steps :: A.Exp Int
-  }
+  } 
 
-type Raymarcher = State RaymarcherState
+type Raymarcher = RWS Config () RaymarcherState
+
+evalRaymarcher :: Raymarcher a -> Config -> RaymarcherState -> a
+evalRaymarcher m c = fst . evalRWS m c
+
+execRaymarcher :: Raymarcher a -> Config -> RaymarcherState -> RaymarcherState
+execRaymarcher m c = fst . execRWS m c
 
 initialRaymarcherState :: Config -> RaymarcherState
-initialRaymarcherState config = RaymarcherState
-  { directionVector = initialDirectionVector config
-  , config = config
-  , positionVector = initialPositionVector config
+initialRaymarcherState config' = RaymarcherState
+  { directionVector = initialDirectionVector config'
+  , positionVector = initialPositionVector config'
   , steps = 0
   }
 
+config :: (Config -> a) -> Raymarcher a
+config = asks
+
+getConfig :: Raymarcher Config
+getConfig = ask
+
 distanceFromStartSq :: Raymarcher (A.Exp Double)
 distanceFromStartSq = do
-  pos <- gets (initialPositionVector . config)
+  pos <- config initialPositionVector 
   pos' <- gets positionVector
   pure $ AL.quadrance (pos' - pos)
 
 hasEscaped :: Raymarcher (A.Exp Bool)
 hasEscaped = do
-  raymarcherConfig <- gets config
-  (maxDistanceSq raymarcherConfig <) <$> distanceFromStartSq
+  maxd <- config maxDistanceSq
+  (maxd <) <$> distanceFromStartSq
 
 hasReachedMaxSteps :: Raymarcher (A.Exp Bool)
 hasReachedMaxSteps = do
-  raymarcherConfig <- gets config
-  (maxSteps raymarcherConfig <) <$> gets steps
+  maxs <- config maxSteps
+  (maxs <) <$> gets steps
 
 hasCollided :: Raymarcher (A.Exp Bool)
 hasCollided = do
-  raymarcherDistance <- gets (distance . shape . config) <*> gets positionVector
-  raymarcherEpsilon <- gets (epsilon . config)
-  pure (raymarcherDistance < raymarcherEpsilon)
+  signedDistance <- config (distance . shape) <*> gets positionVector
+  (signedDistance <) <$> config epsilon
 
 step :: Raymarcher ()
 step = do
   pos <- gets positionVector
   dir <- gets directionVector
-  dist <- gets (distance . shape . config) <*> gets positionVector
+  dist <- config (distance . shape) <*> gets positionVector
   modify $ \s -> s { positionVector = pos + (dir AL.^* dist), steps = steps s + 1 }
 
 getBaseColour :: Raymarcher (A.Exp Colour)
-getBaseColour = gets (colour . shape . config) <*> gets positionVector
+getBaseColour = config (colour . shape) <*> gets positionVector
 
 runRaymarcher :: Config -> A.Exp Colour
-runRaymarcher = evalState raymarcher . initialRaymarcherState
+runRaymarcher config' = evalRaymarcher raymarcher config' (initialRaymarcherState config')
 
 has :: Raymarcher (A.Exp Bool)
 has = foldr1 (||) <$> sequence [hasEscaped, hasReachedMaxSteps, hasCollided]
 
 raymarcher :: Raymarcher (A.Exp Colour)
 raymarcher = do
-  configFog <- gets (fog . config)
+  configFog <- config fog
   whileState (not <$> has) step
   hasEscaped' <- hasEscaped
   col <- getColour
@@ -91,22 +101,25 @@ raymarcher = do
 
 getColour :: Raymarcher (A.Exp Colour)
 getColour = do
-  v1 <- gets (sun . config)
-  v2 <- gets (normal . epsilon . config) <*> gets (shape . config) <*> gets positionVector
+  v1 <- config sun
+  v2 <- config (normal . epsilon) <*> config shape <*> gets positionVector
+  ambient <- config ambientLighting
   lit <- getIsLit
-  ambient <- gets (ambientLighting . config)
   let lightingNormal = (v1 `AL.dot` v2) / (AL.norm v1 * AL.norm v2)
       lightingFactor = if lightingNormal < 0 || not lit
         then A.toFloating ambient
         else A.toFloating $ (1 - ambient)*lightingNormal + ambient
   litBaseColour <- blend (1 - lightingFactor) lightingFactor black <$> getBaseColour
-  fogFactor <- (\d maxd -> if d > maxd then 1.0 else A.toFloating (d/maxd)) <$> distanceFromStartSq <*> gets (maxDistanceSq . config)
-  blend (1 - fogFactor) fogFactor litBaseColour <$> gets (fog . config)
+  fogFactor <- (\d maxd -> if d > maxd then 1.0 else A.toFloating (d/maxd)) <$> distanceFromStartSq <*> config maxDistanceSq
+  blend (1 - fogFactor) fogFactor litBaseColour <$> config fog
 
 getIsLit :: Raymarcher (A.Exp Bool)
-getIsLit = evalState go . f <$> get
+getIsLit = do
+  sun' <- config sun
+  s' <- gets (\s -> s { directionVector = sun', steps = 0})
+  config' <- getConfig
+  pure $ evalRaymarcher go config' s'
   where
-    f s = s { directionVector = sun (config s), steps = 0 }
     go = do
       escapeSurface
       step
@@ -119,27 +132,24 @@ getIsLit = evalState go . f <$> get
 escapeSurface :: Raymarcher ()
 escapeSurface = do
   pos <- gets positionVector
-  eps <- gets (epsilon . config)
-  dir <- (\s -> normal eps s pos) <$> gets (shape . config)
-  dist <- gets (epsilon . config)
-  modify $ \s -> s { positionVector = pos + (dir AL.^* dist) }
+  eps <- config epsilon
+  shape' <- config shape
+  let dir = normal eps shape' pos
+  modify $ \s -> s { positionVector = pos + (dir AL.^* eps) }
 
--- todo: obtain the same effect by removing config from RaymarcherState
-type BareRaymarcher = (A.Exp Point, A.Exp Point, A.Exp Int)
-
-simplify :: RaymarcherState -> BareRaymarcher
-simplify s = (positionVector s, directionVector s, steps s)
-
--- | Given a non-modifying monadic predicate on the state, and a monadic action,
+-- | Given a monadic predicate on the state, and a monadic action,
 -- execute the action repeatedly while the predicate holds.
+-- Any modifications on the state by the predicate are ignored.
 whileState :: Raymarcher (A.Exp Bool) -> Raymarcher () -> Raymarcher ()
 whileState p m = do
   s <- get
-  let -- we assume config remains constant
-      p' (A.T3 pos dir steps) = evalState p s{ positionVector = pos, directionVector = dir, steps = steps }
+  config' <- getConfig
+  let p' (A.T3 pos dir steps) = evalRaymarcher p config' s{ positionVector = pos, directionVector = dir, steps}
       s' = A.lift $ simplify s
       executeOnce (A.T3 pos dir steps) =
         let s'' = s{ positionVector = pos, directionVector = dir, steps = steps }
-         in A.lift . simplify $ execState m s''
+         in A.lift . simplify $ execRaymarcher m config' s''
       (A.T3 endPos endDir endSteps) = A.while p' executeOnce s'
   put s{ positionVector = endPos, directionVector = endDir, steps = endSteps }
+  where
+    simplify s = (positionVector s, directionVector s, steps s)
